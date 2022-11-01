d75ca620636f910e398c21decbb36c5f5e2284a7
HK-Azilen <hardik.kacha@azilen.com>
2022-11-01 15:24:35 +0530

    added more code

--- frontend/src/context/FilterContext.js
+++ frontend/src/context/FilterContext.js
@@ -1,17 +1,12 @@
 import { useState, useEffect, createContext, useCallback } from "react";
 import { read, utils, writeFileXLSX } from "xlsx";
-import { useSelector, useDispatch } from "react-redux";
-import { requestUsers } from "../utils/action";
 
 // import XLFile from "../data/data.xlsx";
-// import XLFile from "../data/77-Jamnagar-Rural -Data.xlsx";
-// import JSONFILE from "../data/77-Jamnagar-Rural-Data.json";
 
 export const FilterContext = createContext();
 
 const FilterProvider = ({ children }) => {
-	const { usersData, isLoading } = useSelector((state) => state);
-	const dispatch = useDispatch();
+	const [result, setResult] = useState([]);
 	const [filteredData, setFilteredData] = useState([]);
 	const [isFiltered, setIsFiltered] = useState(0);
 
@@ -48,7 +43,7 @@ const FilterProvider = ({ children }) => {
 
 	const applyFilter = () => {
 		const filterKeys = Object.keys(selectedFilter);
-		let newUsers = usersData.filter((item) => {
+		let newUsers = result.filter((item) => {
 			return filterKeys.every((key) => {
 				if (!selectedFilter[key].length) return true;
 				return selectedFilter[key].includes(item[key]);
@@ -86,7 +81,7 @@ const FilterProvider = ({ children }) => {
 		}
 	};
 
-	const setFilters = () => {
+  const setFilters = async () => {
 		const PART_NAME_EN_ARRAY = [];
 		const PSBUILDING_NAME_EN_ARRAY = [];
 		const SECTION_NAME_EN_ARRAY = [];
@@ -95,7 +90,7 @@ const FilterProvider = ({ children }) => {
 		const GENDER_ARRAY = [];
 		const PIN_CODE_ARRAY = [];
 
-		usersData.forEach((item, index) => {
+		await result.forEach((item, index) => {
 			PART_NAME_EN_ARRAY.indexOf(item.PART_NAME_EN) === -1 &&
 				PART_NAME_EN_ARRAY.push(item.PART_NAME_EN);
 			PSBUILDING_NAME_EN_ARRAY.indexOf(item.PSBUILDING_NAME_EN) === -1 &&
@@ -123,35 +118,25 @@ const FilterProvider = ({ children }) => {
 		});
 	};
 
-	// const getData = async () => {
-	// const response = await fetch("../data/77-Jamnagar-Rural-Data.json");
-	// const result = await response.json();
-	// const file = await (await fetch(XLFile)).arrayBuffer();
-	// const wb = read(file);
-	// const ws = wb.Sheets[wb.SheetNames[0]];
-	// const data = utils.sheet_to_json(ws);
-	// setResult(JSONFILE);
-	// setFilters(JSONFILE);
-	// };
-
-	// useEffect(() => {
-	// 	// getData();
-	// }, []);
-
-	useEffect(() => {
-		dispatch(requestUsers());
-		setFilters();
-	}, []);
+	const getData = async (fileName) => {
+		const file = await (await fetch(fileName)).arrayBuffer();
+		const wb = read(file);
+		const ws = wb.Sheets[wb.SheetNames[0]];
+		const data = utils.sheet_to_json(ws);
+    console.log(data)
+		setResult(data);
+    setFilters();
+	};
 
 	return (
 		<FilterContext.Provider
 			value={{
-				usersData,
-				isLoading,
+				result,
 				filterList,
 				isFiltered,
 				filteredData,
 				selectedFilter,
+        getData,
 				handleSetFilter,
 				handleExportFile,
 				handleCreateFilter,
